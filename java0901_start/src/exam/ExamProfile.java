3ec08d1d5d59d4208502301fb3aeb61c42dcbd23
ThinkFox <thinkfox89@gmail.com>
2014-09-02 17:27:22 +0900

    9.2

---
+++ java0901_start/src/exam/ExamProfile.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class ExamProfile {
+    
+    public static void main(String[] args) {
+        System.out.println("**********************");
+        System.out.println("* 이름 : 오세현      ");
+        System.out.println("* 나이 : 26          ");
+        System.out.println("* 주소 : 경기도 수원시");
+        System.out.println("*********************");
+        
+    }
+}
